16e5d14ea3412bc717c6f20c59f19699adc2173e
TESTYEE-09 <86476589+TESTYEE-09@users.noreply.github.com>
2026-07-25 22:56:54 +1000

    Load Fireworks key prompt styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
+import './api-key.css';
 import { ApiKeyOverlay } from './components/ApiKeyOverlay.jsx';
 import { ChoiceOverlay } from './components/ChoiceOverlay.jsx';
 import { EndingOverlay } from './components/EndingOverlay.jsx';
